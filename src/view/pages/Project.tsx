e7638182807a3a38679bcc17a96027c7fe24cb2f
AndreaMastracchio <gregorioandrea.mastracchio@gmail.com>
2022-08-22 21:46:41 +0200

    Integrate form state

--- src/view/pages/Project.tsx
+++ src/view/pages/Project.tsx
@@ -2,7 +2,7 @@
 import React, {FC} from 'react';
 
 /* Interface */
-import {Props} from '../../interface/PropsInterface'
+import {Card, Props} from '../../interface/PropsInterface'
 
 /* Layout */
 import ColumnLayout from "../layout/ColumnLayout";
@@ -10,13 +10,61 @@ import ColumnLayout from "../layout/ColumnLayout";
 /* Partials Components */
 import ContactsAccordion from "../partials/ContactsAccordion";
 
-const Project: FC<Props> = () => {
+const Project: FC<Props> = ({}) => {
 
     const folderSection = (
         <div className="md:basis-10/12 grow">
-           <ContactsAccordion />
+            <ContactsAccordion/>
         </div>
     )
+
+    // Cards
+    const cards: Card[] = [
+        {
+            image: 'url(/images/cube-3d.png)',
+            text: 'Testo progetto 1',
+            id_project: 1
+        },
+        {
+            image: 'url(/images/cube-3d.png)',
+            text: 'Testo progetto 2',
+            id_project: 2
+        },
+        {
+            image: 'url(/images/cube-3d.png)',
+            text: 'Testo progetto 3',
+            id_project: 3
+        },
+        {
+            image: 'url(/images/cube-3d.png)',
+            text: 'Testo progetto 3',
+            id_project: 4
+        }
+    ]
+
+    const htmlCard = (item: Card, i: number) => {
+        return (
+            <section id={'project_id_' + item.id_project} key={i}
+                 className="border border-borderLightGray rounded-2xl
+                            flex flex-col gap-4 my-4 mx-4 pb-4">
+            <div>
+                <div className="min-h-[200px] rounded-t-2xl bg-center bg-no-repeat bg-auto"
+                     style={{backgroundImage: item.image}}>
+                </div>
+                <div className="flex flex-col gap-2 px-4">
+                    <p className="text-textLightGray">
+                        item.text
+                    </p>
+                    <button
+                        className="flex justify-center rounded-md items-center px-2 py-1
+                                   text-white bg-[#1C2B3A] w-full lg:w-1/2">
+                        <p className="text-center">view-project</p>
+                    </button>
+                </div>
+            </div>
+        </section>
+        )
+    }
 
     const centralSection = (
         <div className="basis-full">
@@ -28,52 +76,14 @@ const Project: FC<Props> = () => {
                         <p className="text-textLightGray text-sm"> projects </p>
                     </div>
                 </div>
-                <div className="flex flex-col md:flex-row gap-3 md:justify-around h-full md:h-auto">
-                    <div
-                        className="border border-borderLightGray rounded-2xl flex flex-col gap-4 my-4 mx-4 pb-4">
-                        <div className="min-h-[200px] rounded-t-2xl bg-center bg-no-repeat bg-auto"
-                             style={{backgroundImage: "url(/images/cube-3d.png)"}}>
-                        </div>
-                        <div className="flex flex-col gap-2 px-4">
-                            <p className="text-textLightGray">
-                                Duis aute irure dolor in velit esse cillum dolore.
-                            </p>
-                            <button
-                                className="w-1/2 flex justify-center rounded-md items-center px-2 py-1 text-white bg-[#1C2B3A]">
-                                <p className="text-center">view-project</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div
-                        className="border border-borderLightGray rounded-2xl flex flex-col gap-4 my-4 mx-4 pb-4">
-                        <div className="min-h-[200px] rounded-t-2xl bg-center bg-no-repeat bg-auto"
-                             style={{backgroundImage: "url(/images/pc.png)"}}>
-                        </div>
-                        <div className="flex flex-col gap-2 px-4">
-                            <p className="text-textLightGray">
-                                Duis aute irure dolor in velit esse cillum dolore.
-                            </p>
-                            <button
-                                className="w-1/2 flex justify-center rounded-md items-center px-2 py-1 text-white bg-[#1C2B3A]">
-                                <p className="text-center">view-project</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div
-                        className="border border-borderLightGray rounded-2xl flex flex-col gap-4 my-4 mx-4 pb-4">
-                        <div className="min-h-[200px] rounded-t-2xl bg-center bg-no-repeat bg-auto"
-                             style={{backgroundImage: "url(/images/eth.png)"}}>
-                        </div>
-                        <div className="flex flex-col gap-2 px-4">
-                            <p className="text-textLightGray">
-                                Duis aute irure dolor in velit esse cillum dolore.
-                            </p>
-                            <button
-                                className="w-1/2 flex justify-center rounded-md items-center px-2 py-1 text-white bg-[#1C2B3A]">
-                                <p className="text-center">view-project</p>
-                            </button>
-                        </div>
-                    </div>
+                <div className="grid grid-cols-1 md:grid-cols-3">
+                    {
+                        cards.map(function (item: Card, i: number) {
+                            return (
+                                htmlCard(item, i)
+                            )
+                        })
+                    }
                 </div>
             </section>
         </div>
